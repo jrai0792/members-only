f01ffd166cd37aa77e47b805cf4c1defa54d56d3
Jyoti <jyotikiran12@gmail.com>
2020-11-18 16:36:07 +0530

    Fixed linters error

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,21 +1,20 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!, only: %i[:new, :create]
+  before_action :authenticate_user!, only: %i[new create]
 
   # GET /posts
   # GET /posts.json
   def index
     @posts = Post.all
     if user_signed_in?
-      for post in @posts do
-        if post.user_id?
-          user = User.find_by_id(post.user_id)
-          post.user = user
-          
-        end
+      @posts.each do |post|
+        next unless post.user_id?
+
+        user = User.find_by_id(post.user_id)
+        post.user = user
       end
     else
-      for post in @posts do
-          post.user_id = -1
+      @posts.each do |post|
+        post.user_id = -1
       end
     end
   end
@@ -30,18 +29,17 @@ class PostsController < ApplicationController
   def create
     @post = Post.new(post_params)
     @post.user_id = current_user.id
-      if @post.save
-        redirect_to posts_path
-      else
-        render :new
-      end
-  
+    if @post.save
+      redirect_to posts_path
+    else
+      render :new
+    end
   end
 
   private
 
-    # Only allow a list of trusted parameters through.
-    def post_params
-      params.require(:post).permit(:title, :description)
-    end
+  # Only allow a list of trusted parameters through.
+  def post_params
+    params.require(:post).permit(:title, :description)
+  end
 end
